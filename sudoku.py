85608c72bc906ecdc8be8c0cdf85adefffe10d2d
aayushDaftary <aayush.daftary@gmail.com>
2024-05-03 13:27:15 -0400

    finished write to file for output and check for an empty square

--- sudoku.py
+++ sudoku.py
@@ -1,10 +1,24 @@
 def intialize_board(fileName):
-        with open(fileName, 'r') as file:
-                lines = file.readlines()
-                board = []
-                for line in lines:
-                        board_row = []
-                        for num in line.split():
-                                board_row.append(int(num))
-                        board.append(board_row)
-                return board
+        file = open(fileName, 'r')
+        lines = file.readlines()
+        board = []
+        for line in lines:
+                board_row = []
+                for num in line.split():
+                        board_row.append(int(num))
+                board.append(board_row)
+        file.close()
+        return board
+
+def board_to_file(board, fileName):
+        file = open(fileName, 'w')
+        for row in board:
+                file.write(' '.join(map(str,row)) + '\n')
+        file.close()
+
+def empty_square(board):
+        for i in range(len(board)-1):
+                for j in range(len(board[0]) -1 ):
+                        if board[i][j] == 0:
+                                return i, j
+        return None
