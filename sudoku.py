63a35af1a77fde4786c0bc1b3a0aa70e2bef4b8a
Nate-Smithline <nathan_m_smith@outlook.com>
2024-05-07 21:28:08 -0400

    final version checked and established

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,4 @@
+import math
 """
 Artificial Intelligence Project 2, Sodoku
 
@@ -6,161 +7,187 @@ Completed by Aayush Draftary and Nathan Smith
 
 
 class Sodoku:
-        """
-        INIT FUNCTION
+    """
+    INIT FUNCTION
 
-        - will establish the initial variables and run the entire program front-to-back
-        """
-        def __init__(self, import_file, export_file):
-                self.game_board = []
+    - will establish the initial variables and run the entire program front-to-back
+    """
+    def __init__(self, import_file, export_file):
+        #the game board itself, array of arrays
+        self.game_board = []
 
-                #initialize the board
-                self.board_read_in(import_file)
+        #read the board in
+        self.board_read_in(import_file)
 
-                self.play_sudoku()
+        #play the game
+        self.play_sudoku()
 
-                self.board_read_out(export_file)
+        #read it out
+        self.board_read_out(export_file)
         
-        """
-        BOARD READ IN
+    """
+    BOARD READ IN
 
-        will read the file sent through the program into a gameBoard that will be used to do all the work
-        """
-        def board_read_in(self, import_file):
-                file = open(import_file, 'r')
-                lines = file.readlines()
-                #go through each line
-                for line in lines:
-                        board_row = []
-                        for num in line.split():
-                                #split and add to master gameboard array
-                                board_row.append(int(num))
-                        self.game_board.append(board_row)
-                file.close()
+    will read the file sent through the program into a gameBoard that will be used to do all the work
+    """
+    def board_read_in(self, import_file):
+        file = open(import_file, 'r')
+        lines = file.readlines()
+        
+        #go through all the lines
+        for line in lines:
 
-        """
-        BOARD READ OUT
-        """
-        def board_read_out(self, export_file):
-                file = open(export_file, 'w')
-                #go through each row and write it back in
-                for row in self.game_board:
-                        file.write(' '.join(map(str,row)) + '\n')
-                file.close()
+            board_row = []
+            
+            for num in line.split():
+                #split and add to master gameboard array
+                board_row.append(int(num))
+
+            self.game_board.append(board_row)
+
+        file.close()
+
+    """
+    BOARD READ OUT
+
+    This takes the gameboard and reads it back into the destination file. Granted this could be the same file, but must be specified. 
+    """
+    def board_read_out(self, export_file):
+        file = open(export_file, 'w')
+
+        #go through line by line and read back into the file.
+        for row in self.game_board:
+            file.write(' '.join(map(str,row)) + '\n')
+
+        file.close()
 
 
 
-        """
-        PLAY_SODOKU
+    """
+    PLAY_SODOKU
 
-        This is the master function that runs the backtracking and goes through everything 
-        """
-        def play_sudoku(self):
-                empty = self.find_empty()
-                if not empty:
-                        return True
-                row, col = empty
+    This is the master function that runs the backtracking and goes through everything 
+    """
+    def play_sudoku(self):
+        #the empty calls the heuristic methods, but not in the recursive backtracking function
+        empty = self.find_empty()
+        if empty:
+            row, col = empty
+        else:
+            return True
+        
+        #go in order, except those that are used already
+        for num in self.domain_order(row, col):
+            #look for valid values
+            if self.valid_val(row, col, num):
+                #set and recursively call
+                self.game_board[row][col] = num
+                if self.play_sudoku():
+                    return True
+                else:
+                    #backtracking
+                    self.game_board[row][col] = 0
+        return False
 
-                for num in range(1,10):
-                        if self.valid_value(row, col, num):
-                                self.game_board[row][col] = num
-                                if self.play_sudoku():
-                                        return True
-                                else:
-                                        self.game_board[row][col] = 0
+    """
+    Find Empty
+
+    This is going to go throughout the gameboard and find the first item that it can that is 0, then return that value
+    """
+    def find_empty(self):
+        #go through each row
+        for i, row in enumerate(self.game_board):
+            #go through each item in each row
+            for j, num in enumerate(row):
+                #return inf match
+                if num == 0:
+                    return i, j
+        return False # was either False or None, doesn't make a huge difference
+    
+
+    """
+    Valid Value
+
+    This valid value system is going to check if the theorized solution has a match already in the order system that could cause a problem. It is essentially a gut checking system throughout the program
+    """    
+    def valid_val(self, row, col, num):
+        #basic check if it's in the same row or column
+        for i in range(9):
+            if self.game_board[row][i] == num or self.game_board[i][col] == num:
                 return False
 
-        """
-        FIND EMPTY
+        #check for a diagonal match
+        for i in range(3):
+            for j in range(3):
+                #had to Google how to do a diagonal match, luckily lots of blocks
+                if self.game_board[3 * (row // 3) + i][3 * (col // 3) + j] == num:
+                    return False
 
-        this function is going to look through the board and find an empty square
-        """
-        def find_empty(self):
-                min_values = float('inf')
-                max_constraints = -1
-                selected_cell = None
-                
-                for i, row in enumerate(self.game_board):
-                        for j, num in enumerate(row):
-                                if num == 0:
-                                        num_values = self.mrv(i, j)
+        return True
 
-                                        constraints = self.dh_calculate(i, j)
-                                        
-                                        if num_values < min_values or (num_values == min_values and constraints > max_constraints):
-                                                min_values = num_values
-                                                max_constraints = constraints
-                                                selected_cell = (i, j)
-                return selected_cell                
+    """
+    domain_order
+
+    This function goes through the domain order originally in chronological order, but after trial also adds into a set all the numbers it could not be to eliminate that before returning the right answer
+
+    This is also the most important because it is where we decided to use the degree heuristic and minimum remaining variable
+    """
+    def domain_order(self, row, col):
+
+        used_nums = []
+
+        for all_indxs in range(9):
+            used_nums.append(self.game_board[row][all_indxs])
+            used_nums.append(self.game_board[all_indxs][col])
+            used_nums.append(self.game_board[3 * (row // 3) + all_indxs // 3][3 * (col // 3) + all_indxs % 3])
+        
+        possible_values = []
+        for indx in range(1, 10):
+            if indx not in used_nums:
+                possible_values.append(indx)
+
+        # found online this was a good way to do the balancing, works beautifully and logically makes sense
+        possible_values.sort(key=lambda x: (self.mrv(row, col, x), self.dh(row, col)))
+
+        return possible_values
 
 
-        """
-        MINIMUM REMAINING VALUES
+    """
+    MINIMUM REMAINING VARIABLE
 
-        This function calculates the number of values remaining in the entire setup
-        """
-        def mrv(self, row, col):
-                # set is more optimized for time complexity
-                used_nums = set()
-                
-                #calculate all related
-                for i in range(9):
-                        # add everything in the column
-                        used_nums.add(self.game_board[row][i])
-                        #add everything in the row
-                        used_nums.add(self.game_board[i][col])
-                        #add diagonals
-                        used_nums.add(self.game_board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3])
+    This function is going to calculate the number of close restraints to factor this into the equation
+    """
+    def mrv(self, row, col, num):
+        num_cnstrnts = 0
+        for i in range(9):
+            
+            #add constraint based on horizontal or vertical
+            if self.game_board[row][i] == num or self.game_board[i][col] == num:
+                num_cnstrnts += 1
 
-                        if row == col:
-                                used_nums.add(self.game_board[i][i])
-                        if row + col == 8:
-                                used_nums.add(self.game_board[i][8 - i])
+            #add constraint based on 45 or 135 degrees
+            if self.game_board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] == num:
+                num_cnstrnts += 1
+        return num_cnstrnts
 
-                possible_values = [num for num in range(1, 10) if num not in used_nums]
-                return len(possible_values)
+
+    """
+    Degree Heuristic
+
+    Calculates the number of things that would be affected by a choice on the one
+    """
+    def dh(self, row, col):
+        affected_degrees = 0
+        for indx in range(9):
+            if self.game_board[row][indx] == 0:
+                affected_degrees += 1
+            if self.game_board[indx][col] == 0:
+                affected_degrees += 1
+            if self.game_board[3 * (row // 3) + indx // 3][3 * (col // 3) + indx % 3] == 0:
+                affected_degrees += 1
+        return affected_degrees
         
 
-        """
-        DH CALCULATE
-
-        This function is going to calculate the degree heuristic (or number of items that are based on this)
-        
-        """
-        def dh_calculate(self, row, col):
-                num_constraints = 0
-                for indx in range(9):
-                        if self.game_board[row][indx] != 0:
-                                num_constraints += 1
-                        if self.game_board[indx][col] != 0:
-                                num_constraints += 1
-                        if self.game_board[3 * (row // 3) + indx // 3][3 * (col // 3) + indx % 3] != 0:
-                                num_constraints += 1
-                        if row == col and self.game_board[indx][indx] != 0:
-                                num_constraints += 1
-                        if row + col == 8 and self.game_board[indx][8 - indx] != 0:
-                                num_constraints += 1
-                
-                return num_constraints
 
 
-        """
-        VALID_VALUE
-
-        This function checks if the value found is valid in line. It is going search the row it is in, and try to find an error in it
-        """
-        def valid_value(self, row, col, num):
-                for i in range(9):
-                        if (self.game_board[row][i] == num or  self.game_board[i][col] == num):
-                                return False
-                        elif(self.game_board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] == num): #3x3 block
-                                return False
-                        elif(row == col and self.game_board[i][i] == num): #45 diagonal
-                                return False
-                        elif((row + col == 8 and self.game_board[i][8 - i] == num)):  # Check 135 diagonal
-                                return False
-                return True
-
-
-
-sdk = Sodoku('Sample_Input.txt', 'SampleIO.txt')
+sdk = Sodoku('Input3.txt', 'Input3_Out.txt')
