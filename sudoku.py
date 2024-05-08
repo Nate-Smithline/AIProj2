fd961db1162774aa91bf460feb7d77eb91f5c9b5
Nate-Smithline <nathan_m_smith@outlook.com>
2024-05-08 19:17:43 -0400

    fixed solution to work, without comments but works for example problem

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,3 @@
-import math
 """
 Artificial Intelligence Project 2, Sodoku
 
@@ -24,7 +23,8 @@ class Sodoku:
 
         #read it out
         self.board_read_out(export_file)
-        
+
+
     """
     BOARD READ IN
 
@@ -69,16 +69,18 @@ class Sodoku:
     This is the master function that runs the backtracking and goes through everything 
     """
     def play_sudoku(self):
-        #the empty calls the heuristic methods, but not in the recursive backtracking function
-        empty = self.find_empty()
+        # this function is called to see if there are any more moves, and to potentially end the game
+    
+        empty = self.find_next()
         if empty:
             row, col = empty
         else:
             return True
-        
+    
         #go in order, except those that are used already
-        for num in self.domain_order(row, col):
+        for num in self.domain_order():
             #look for valid values
+
             if self.valid_val(row, col, num):
                 #set and recursively call
                 self.game_board[row][col] = num
@@ -87,22 +89,135 @@ class Sodoku:
                 else:
                     #backtracking
                     self.game_board[row][col] = 0
+        
+        
         return False
+
+
+    """
+    Remaining Values (RV)
+
+    This function is the backbone of the mrv formula. It's job is to find the remaining values for a specific coordinate point
+    """
+    def rv(self, x, y):
+        all_vals = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+        for i in range(9):
+            #values in other cols
+            if self.game_board[x][i] in all_vals:
+                all_vals.remove(self.game_board[x][i])
+
+            #values in other rows
+            if self.game_board[i][y] in all_vals:
+                all_vals.remove(self.game_board[i][y])
+        
+
+        # Values in diagonal ends
+        if x == y:  # Top-left to bottom-right diagonal
+            for i in range(9):
+                if self.game_board[i][i] in all_vals:
+                    all_vals.remove(self.game_board[i][i])
+
+        if x + y == 8:  # Top-right to bottom-left diagonal
+            for i in range(9):
+                if self.game_board[i][8 - i] in all_vals:
+                    all_vals.remove(self.game_board[i][8 - i])
+
+        return len(all_vals)
+
+
+    """
+    MINIMUM REMAINING VARIABLE
+
+    This function is going to calculate the number of close restraints to factor this into the equation
+    """
+    def mrv(self, options):
+
+        finalists = []
+        final_rv = float("inf")
+
+        for (x, y) in options:
+            val = self.rv(x, y)
+            if(val == final_rv):
+                finalists.append((x, y))
+            elif(val < final_rv):
+                finalists = []
+                finalists.append((x, y))
+                final_rv = val
+
+        return finalists
+        
+
+
+    def degrees(self, x, y):
+        num_cnstrnts = 0
+        
+        for i in range(9):
+            #values in other cols
+            if self.game_board[x][i] != 0:
+                num_cnstrnts += 1
+
+            #values in other rows
+            if self.game_board[i][y] != 0:
+                num_cnstrnts += 1
+        
+
+        # Values in diagonal ends
+        if x == y:  # Top-left to bottom-right diagonal
+            for i in range(9):
+                if self.game_board[i][i] != 0:
+                    num_cnstrnts += 1
+
+        if x + y == 8:  # Top-right to bottom-left diagonal
+            for i in range(9):
+                if self.game_board[i][8 - i] != 0:
+                    num_cnstrnts += 1
+
+        return num_cnstrnts
+
+    """
+    Degree Heuristic
+
+    This calculates the number of constraints on itself
+    """
+    def dh(self, options):
+        finalists = []
+        final_deg = float("inf")
+
+        for (x, y) in options:
+            val = self.degrees(x, y)
+            if(val == final_deg):
+                finalists.append((x, y))
+            elif(val < final_deg):
+                finalists = []
+                finalists.append((x, y))
+                final_deg = val
+
+        return finalists
+
 
     """
     Find Empty
 
     This is going to go throughout the gameboard and find the first item that it can that is 0, then return that value
     """
-    def find_empty(self):
+    def find_next(self):
         #go through each row
-        for i, row in enumerate(self.game_board):
-            #go through each item in each row
-            for j, num in enumerate(row):
-                #return inf match
-                if num == 0:
-                    return i, j
-        return False # was either False or None, doesn't make a huge difference
+        options = []
+
+        for x in range(len(self.game_board)):
+            for y in range(len(self.game_board[x])):
+                if(self.game_board[x][y] == 0):
+                   options.append((x, y)) 
+        if(len(options) == 0):
+            return False
+
+        mrvs = self.mrv(options)
+        if(len(mrvs) >= 2):
+            dhs = self.dh(mrvs)
+            return dhs[0]
+        return mrvs[0]
+
     
 
     """
@@ -115,6 +230,7 @@ class Sodoku:
         for i in range(9):
             if self.game_board[row][i] == num or self.game_board[i][col] == num:
                 return False
+            
 
         #check for a diagonal match
         for i in range(3):
@@ -123,71 +239,33 @@ class Sodoku:
                 if self.game_board[3 * (row // 3) + i][3 * (col // 3) + j] == num:
                     return False
 
+                
+        if row == col:  # Top-left to bottom-right diagonal
+            for indx in range(9):
+                if self.game_board[indx][indx] == num:
+                    return False
+
+        if row + col == 8:  # Top-right to bottom-left diagonal
+            for indx in range(9):
+                if self.game_board[indx][8 - indx] == num:
+                    return False
+
         return True
+
 
     """
     domain_order
 
-    This function goes through the domain order originally in chronological order, but after trial also adds into a set all the numbers it could not be to eliminate that before returning the right answer
-
-    This is also the most important because it is where we decided to use the degree heuristic and minimum remaining variable
+    This function could eventually be changed for more complex operations, but right now just goes in order from 1-9
     """
-    def domain_order(self, row, col):
-
-        used_nums = []
-
-        for all_indxs in range(9):
-            used_nums.append(self.game_board[row][all_indxs])
-            used_nums.append(self.game_board[all_indxs][col])
-            used_nums.append(self.game_board[3 * (row // 3) + all_indxs // 3][3 * (col // 3) + all_indxs % 3])
-        
-        possible_values = []
-        for indx in range(1, 10):
-            if indx not in used_nums:
-                possible_values.append(indx)
-
-        # found online this was a good way to do the balancing, works beautifully and logically makes sense
-        possible_values.sort(key=lambda x: (self.mrv(row, col, x), self.dh(row, col)))
-
-        return possible_values
+    def domain_order(self):
+        return range(1, 10)
 
 
-    """
-    MINIMUM REMAINING VARIABLE
 
-    This function is going to calculate the number of close restraints to factor this into the equation
-    """
-    def mrv(self, row, col, num):
-        num_cnstrnts = 0
-        for i in range(9):
-            
-            #add constraint based on horizontal or vertical
-            if self.game_board[row][i] == num or self.game_board[i][col] == num:
-                num_cnstrnts += 1
-
-            #add constraint based on 45 or 135 degrees
-            if self.game_board[3 * (row // 3) + i // 3][3 * (col // 3) + i % 3] == num:
-                num_cnstrnts += 1
-        return num_cnstrnts
-
-
-    """
-    Degree Heuristic
-
-    Calculates the number of things that would be affected by a choice on the one
-    """
-    def dh(self, row, col):
-        affected_degrees = 0
-        for indx in range(9):
-            if self.game_board[row][indx] == 0:
-                affected_degrees += 1
-            if self.game_board[indx][col] == 0:
-                affected_degrees += 1
-            if self.game_board[3 * (row // 3) + indx // 3][3 * (col // 3) + indx % 3] == 0:
-                affected_degrees += 1
-        return affected_degrees
+    
         
 
 
 
-sdk = Sodoku('Input3.txt', 'Input3_Out.txt')
+sdk = Sodoku('Sample_Input.txt', 'Sample_Output.txt')
